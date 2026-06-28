42e16f4d8bc9e62b508fa49646a385e1dc89f149
Faustino Rodriguez <Faustinorodriguez0410@gmail.com>
2026-06-28 17:35:02 -0300

    Creacion de la funcion que contiene el menú e importacion de los archivos de juegos

---
+++ menu.py
@@ -0,0 +1,17 @@
+#Importacion de los modulos de juegos
+import juego1
+import juego2
+import juego3
+import juego4
+
+#Creo la funcion que contiene el menú
+def mostrarMenu():
+    #Mensaje de bienvenida al usuario y opciones a elegir
+    print("\n" + "*" * 30)
+    print("   BIENVEIDOS A PLAYMATIC   ")
+    print("*" * 30)
+    print("1- Juego1")
+    print("2- juego2")
+    print("3- juego3")
+    print("4- juego4")
+    print("5. Salir")
