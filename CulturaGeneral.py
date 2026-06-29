8aa0b659bbd53f59fc28fb94c63f82dd30c09e4f
Benicio Guzman <beniciog29@gmail.com>
2026-06-29 08:51:56 -0300

    comentarios

--- CulturaGeneral.py
+++ CulturaGeneral.py
@@ -2,9 +2,10 @@ def guardarRanking(nombre, puntaje):
     """Guarda el puntaje en un archivo de texto."""
     with open("rankingCultura.txt", "a") as archivo:
         archivo.write(f"Jugador: {nombre} - Puntaje: {puntaje} puntos\n")
-
+# Función principal del juego de Cultura General
 def cultura_general():
     """Lógica principal del juego."""
+    # Lista que contiene las preguntas, las opciones y la respuesta correcta
     preguntas = [
         {"pregunta": "¿Cuál es el planeta más grande del Sistema Solar?", "opciones": ["Marte", "Júpiter", "Venus", "Saturno"], "respuesta": 2},
         {"pregunta": "¿Cuántos continentes hay en el mundo?", "opciones": ["5", "6", "7", "8"], "respuesta": 3},
@@ -12,14 +13,17 @@ def cultura_general():
         {"pregunta": "¿Cuál es la capital de Argentina?", "opciones": ["Rosario", "Córdoba", "Buenos Aires", "Mendoza"], "respuesta": 3},
         {"pregunta": "¿Qué océano es el más grande del planeta?", "opciones": ["Atlántico", "Índico", "Pacífico", "Ártico"], "respuesta": 3}
     ]
+     # Variable donde se acumula el puntaje
     puntaje = 0
 
     print("\n--- ¡A JUGAR! ---")
+     # Recorre todas las preguntas de la lista
     for i, p in enumerate(preguntas, start=1):
         print(f"\nPregunta {i}: {p['pregunta']}")
+         # Muestra las opciones de respuesta
         for j, op in enumerate(p['opciones'], start=1):
             print(f"{j}. {op}")
-        
+        # Valida que el usuario ingrese un número entre 1 y 4
         while True:
             try:
                 resp = int(input("Ingrese su respuesta (1-4): "))
@@ -28,15 +32,17 @@ def cultura_general():
                 print("Debe ingresar un número entre 1 y 4.")
             except ValueError:
                 print("Ingrese un número válido.")
-
+        # Verifica si la respuesta es correcta 
         if resp == p['respuesta']:
             print("¡Correcto!")
             puntaje += 20
         else:
             print(f"Incorrecto. La respuesta era: {p['opciones'][p['respuesta']-1]}")
-
+         # Muestra el resultado fina
     print(f"\nPuntaje final: {puntaje} de 100")
+    # Solicita el nombre del jugador
     nombre = input("Ingrese su nombre para el ranking: ")
+     # Guarda el nombre y el puntaje en el archivo rankingCultural.txt
     guardarRanking(nombre, puntaje)
 
 def verRanking():
@@ -61,7 +67,7 @@ def mostrarMenu():
     print("2 - Ver Ranking")
     print("3 - Volver al Menú Principal")
     print("=" * 40)
-
+# Solicita al usuario que escriba el comando para iniciar el juego
 def iniciarJuego():
     """Función que llama el menu.py"""
     while True:
